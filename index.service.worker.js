4417a3e532852cdbc5425daf83ef863442cbc671
Gravedigger7789 <Gravedigger7789@users.noreply.github.com>
2022-11-02 03:50:43 +0000

    deploy: b4ca0c1e8e8dddbfa783813711af5721c2ee98d9

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1667359817|3242830";
+const CACHE_VERSION = "1667361037|2321377";
 const CACHE_PREFIX = "godot-actions-sa-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
